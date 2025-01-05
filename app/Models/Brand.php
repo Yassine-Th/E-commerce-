a2df9cdccbb3ad83caab445861d29a0d9bfd7e81
Yassine <yassine.tihmmou@gmail.com>
2025-01-05 16:37:07 +0100

    2

--- app/Models/Brand.php
+++ app/Models/Brand.php
@@ -5,16 +5,14 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class Category extends Model
+class Brand extends Model
 {
     use HasFactory;
 
     protected $fillable = [
-        'name',
-        'slug',
-        'image',
-        'is_active'
+        '*'
     ];
+    
     public function products()
     {
         return $this->hasMany(Product::class);
